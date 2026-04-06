00eee688a59fe9b951481754f23dd77cd905860f
MegathilagavathyS <megasubu930@gmail.com>
2026-04-06 17:55:03 +0530

    java basics

---
+++ src/basic_tutorials/Arrays_basics.java
@@ -0,0 +1,26 @@
+package basic_tutorials;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Arrays_basics {
+    public static void main(String[] args) {
+        ArrayList<Integer> list= new ArrayList<>();
+        Scanner in=new Scanner(System.in);
+        int [] arr1= new int[5];
+        for(int i=0;i< arr1.length;i++){
+            arr1[i]=in.nextInt();
+        }
+        for(int i=0;i<arr1.length;i++){
+            System.out.print(arr1[i] + " ");
+        }
+        System.out.println();
+        for(int num: arr1){
+            System.out.print(num+" ");
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(arr1));
+
+    }
+}
